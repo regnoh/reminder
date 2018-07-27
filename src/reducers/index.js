eda670c582522b52ffbdff64da15ddd0d48268b7
regnoh <algd2458>
2018-07-27 23:50:58 +0800

    sfcookie

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import {ADD_REMINDER,DELETE_REMINDER,CLEAR_REMINDERS} from "../constants";
-
+import { bake_cookie, read_cookie } from 'sfcookies'
 const reminder =(action)=>{
     const {text,dueDate}=action;
     return {//新纪录
@@ -8,18 +8,24 @@ const reminder =(action)=>{
         id:Math.random()
     }
 }
-const reminders = (state=[],action={})=>{
+const reminders = (state=read_cookie("reminders")||[],action={})=>{
+    let reminders = null;
     switch(action.type){
         case ADD_REMINDER:
-            return [//数组
+            reminders = [//数组
                 ...state,//原纪录
                 reminder(action)//新纪录
-            ]
+            ];
+            bake_cookie("reminders",reminders);
+            return reminders;
         case DELETE_REMINDER:
-        // 返回id对应的其他reminder
-            return state.filter(reminder=>reminder.id !== action.id)
+            reminders = state.filter(reminder=>reminder.id !== action.id);// 返回id对应的其他reminder
+            bake_cookie("reminders",reminders);
+            return reminders;
         case CLEAR_REMINDERS:
-            return []
+            reminders = []
+            bake_cookie("reminders",reminders);
+            return reminders;
         default:return state;
     }
 }
